5a49324a2cb91152d105413fcb80d5bd8a472e53
goddhi <godwillcharles22@gmail.com>
2024-01-26 23:46:02 +0100

    testing app

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -3,9 +3,12 @@ package config
 // these packages are used to connect to mysql database
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	// "github.com/jinzhu/gorm" for gorm v1
+	//"github.com/jinzhu/gorm/dialects/mysql"
+	// _ "github.com/jinzhu/gorm/dialects/mysql" // for gorm V1
+	"gorm.io/driver/mysql" // for gorm V2
+	"gorm.io/gorm" // for gorm V2
+
 )
 
 // the main reason for this file is to return a variable called db so other files can interact with DB
@@ -15,20 +18,25 @@ var (
 )
 
 func Connect(){  // this functions helps to open connection with the mysql database 
-	// connecting to database using gorm V1
+	// connecting to mysql database using gorm V1
+
 	// d, err := gorm.Open("mysql", "goddhi:goddhipassword%4012@/simplerest?charset=utf8&parseTime=True&loc=Local") // specifying the database username, password, table name
 	// if err != nil { // if an error occurs 
 	// 	// In this example, if gorm.Open fails to  clonnect to the database, the function will panic, printing the error message and stopping the execution of the program.
 	// 	panic(err)  // panic is called, it immediately stops the execution of the function in which it was called and begins to unwind the stack, running any deferred functions along the way. If the panic is not recovered, it eventually stops the entire program. The statement panic(err) is commonly used to handle unexpected errors where the program cannot (or should not) continue its execution. Here's a basic breakdown:   err: This represents an error value. In Go, error handling is usually done by returning an error type as the last value in a function's return list. If an error is not nil, it indicates that something went wrong.  panic(err): This line triggers a panic with the error err. It's often used in situations where the error is critical and there is no way for the program to recover or proceed further. For example, if your program absolutely needs to read a configuration file to start, and it can't find that file, you might use panic to halt the program. 
 	// }   
-	// connecting to database using gorm V2
-	dsn := "goddhi:goddhipassword%4012@tcp(localhost:3306)/simplerest?charset=utf8&parseTime=True&Loc=Local"
-	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	db = d  // passing the value of d to db variable.....
-} 
 
+	// connecting to mysql database using gorm V2
+	dsn := "goddhi:goddhipassword@tcp(localhost:3306)/simplerest?charset=utf8&parseTime=True&loc=Local"
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err) // // panic is called, it immediately stops the execution of the function in which it was called and begins to unwind the stack, running any deferred functions along the way. If the panic is not recovered, it eventually stops the entire program. The statement panic(err) is commonly used to handle unexpected errors where the program cannot (or should not) continue its execution. Here's a basic breakdown:   err: This represents an error value. In Go, error handling is usually done by returning an error type as the last value in a function's return list. If an error is not nil, it indicates that something went wrong.  panic(err): This line triggers a panic with the error err. It's often used in situations where the error is critical and there is no way for the program to recover or proceed further. For example, if your program absolutely needs to read a configuration file to start, and it can't find that file, you might use panic to halt the program.
+	}
+	db = d 
+} 
+/// 
 func GetDb() *gorm.DB{  // this function returns db which connect to the mysql database running locally  to other folders
 	return db
 }
 
-/// .....
+/// testing connectivity 
--- pkg/models/books.go
+++ pkg/models/books.go
@@ -1,8 +1,10 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
+	// "github.com/jinzhu/gorm" gorm v1
+	"log"
 	"github.com/goddhi/book-management-system/pkg/config" // importing the config folder so that this file can connect to the database
+	"gorm.io/gorm"                                        // for gorm V2
 )
 
 var db *gorm.DB
@@ -11,7 +13,7 @@ var db *gorm.DB
 // this struct are created as module or table to store things in the database
 type Book struct{
 	gorm.Model
-	Name string `gorm:""json:"name"`
+	Name string `gorm:"" json:"name"`
 	Author string `json:"author"`
 	Publication string `json:"publication"`
 }
@@ -25,8 +27,19 @@ func init()  {  // using the init function to initialize database with the conne
 /// All functions below will be use to interact with the database
 
 func (b *Book) CreateBook() *Book{ // passing the Book(struct) as a parameter and returning the Book(struct) as b. 
-	db.NewRecord(b) // db which is used to connect to the database and NewRecord which is gotten from the gorm(ORM) is used to create a record based on the Book struct 
-	db.Create(&b) // db which is used to connect to the database and NewRecord which is gotten from the gorm(ORM) is used to create a table based on the Book struct 
+	
+
+
+	/// this is used for gorm v1
+	//db.NewRecord(b) // db which is used to connect to the database and NewRecord which is gotten from the gorm(ORM) is used to create a record based on the Book struct 
+	//  In GORM V2, there's no need to check if it's a new record with `NewRecord`.
+    // Directly call the Create method.
+	
+	// This code assumes that db is your database connection instance. The Create method in GORM V2 automatically handles new records and assigns primary keys as needed.
+	result := db.Create(&b) // db which is used to connect to the database and NewRecord which is gotten from the gorm(ORM) is used to create a table based on the Book struct 
+	if result.Error != nil {
+		log.Fatal("Error trying to cretae database table")
+	}
 	return b  /// returning the Book struct as b
 }
 
@@ -49,4 +62,3 @@ func DeleteBook(ID int64) Book {  // function that delete a book ID and returns 
 	db.Where("ID=?", ID).Delete(book) // using  db to delete a book ID
 	return book
 }
-
